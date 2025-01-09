c7a2e4a6a974d7ea7cc7c3a94d712c33244a295b
JulioBendz <bengutjul2@gmail.com>
2025-01-09 02:14:49 -0500

    Se agregó Services, Inyección de Dependencias, This y Routing según documentación de angular 19

--- src/app/components/housing-location/housing-location.component.ts
+++ src/app/components/housing-location/housing-location.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
-import { Housinglocation } from '../housinglocation';
+import { HousingLocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
 
-  @Input() housingLocation!: Housinglocation;
+  @Input() housingLocation!: HousingLocation;
 }
